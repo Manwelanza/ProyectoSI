01925714562781d3e492d4ff5ce0bb9008dd2082
joseka1234 <joseka1234@gmail.com>
2015-11-13 16:36:42 +0100

    Enemigo persigue, problema con paredes

---
+++ Assets/Scripts/MovimientoAutomatico.cs
@@ -0,0 +1,23 @@
+﻿using RAIN.Core;
+using UnityEngine;
+
+public class MovimientoAutomatico : MonoBehaviour {
+
+	[SerializeField]
+	private Vector3 destino;
+	private AIRig aiRig = null;
+
+	void Start() {
+		aiRig = GetComponentInChildren<AIRig> ();
+	}
+
+	void Update() {
+		destino = GameObject.Find ("Player").transform.position;
+		aiRig.AI.Motor.UpdateMotionTransforms ();
+		aiRig.AI.Motor.MoveTarget.VectorTarget = destino;
+		aiRig.AI.Motor.Move ();
+
+		aiRig.AI.Motor.ApplyMotionTransforms ();
+	}
+
+}
